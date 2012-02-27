777b01c8bdec32345d78e04e85bc709ebd8f86df
Nick Howes <nick@nickhowes.co.uk>
2012-02-27 20:21:07 +0000

    Replace settings screen entirely.
    It is now a custom Activity with some explanatory text so that
    you don't have to guess what format the images should be.
    Still a lot of room for improvement.
    
    Also a quick icon instead of the placeholder.

--- src/uk/co/halfninja/wallpaper/parallax/ParallaxWallpaperSettings.java
+++ src/uk/co/halfninja/wallpaper/parallax/ParallaxWallpaperSettings.java
@@ -1,165 +1,113 @@
 package uk.co.halfninja.wallpaper.parallax;
 
-import java.io.File;
-
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.preference.EditTextPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.Preference.OnPreferenceClickListener;
-import android.preference.PreferenceActivity;
 import android.provider.MediaStore;
+import android.text.Html;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import static uk.co.halfninja.wallpaper.parallax.ParallaxWallpaper.TAG;
 
-/**
- * TODO this setting screen utterly sucks
- */
-public class ParallaxWallpaperSettings extends PreferenceActivity
-    implements SharedPreferences.OnSharedPreferenceChangeListener {
-
-    private static final String CUSTOM_PATH_KEY = "custom_path";
+public class ParallaxWallpaperSettings extends Activity implements OnSharedPreferenceChangeListener {
     public static final String CUSTOM_PATH_ACTUAL_KEY = "custom_path_actual";
     private static final int REQ_CODE_PICK_IMAGE = 100001;
-    private EditTextPreference customPathPreference;
+    
+    private final Context context = this; // for convenience in inner classes
+    
     private SharedPreferences preferences;
-
-    @Override
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        getPreferenceManager().setSharedPreferencesName(
-                ParallaxWallpaper.SHARED_PREFS_NAME);
-        addPreferencesFromResource(R.xml.settings);
-        preferences = getPreferenceManager().getSharedPreferences();
-        preferences.registerOnSharedPreferenceChangeListener(this);
-        
-        final String actualPath = preferences.getString(CUSTOM_PATH_ACTUAL_KEY, Environment.getExternalStorageDirectory().getAbsolutePath());
-        
-        if (preferences.getString(CUSTOM_PATH_KEY, null) == null) {
-            
-        }
-        
-        customPathPreference = (EditTextPreference) getPreferenceScreen().findPreference(CUSTOM_PATH_KEY);
-        if (hasText(actualPath)) {
-            customPathPreference.setSummary(actualPath);
-            customPathPreference.setText(actualPath);
-        }
-        
-        if (!hasText(customPathPreference.getText())) {
-            resetPathPreference();
-        }
-        customPathPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-            /**
-             * Start any app that supports the openintents.org PICK_FILE intent,
-             * such as File Manager
-             */
-            @Override public boolean onPreferenceClick(Preference preference) {
-                Intent anyFile = new Intent(Intent.ACTION_GET_CONTENT);
-                anyFile.setType("image/*");
-                anyFile.addCategory(Intent.CATEGORY_OPENABLE);
-                
-                if (anyFile.resolveActivityInfo(getPackageManager(), 0) == null) {
-                    return false;
-                } else {
-                    Intent chooser = Intent.createChooser(anyFile, "Pick the first layer");
-                    startActivityForResult(chooser, REQ_CODE_PICK_IMAGE);
-                    return true;
-                }
+	private Button pickButton;
+	private TextView selectedImage;
+	
+	public String getLayerPath() {
+		return preferences.getString(CUSTOM_PATH_ACTUAL_KEY, null);
+	}
+	
+	public void setLayerPath(String path) {
+		preferences.edit().putString(CUSTOM_PATH_ACTUAL_KEY, path).commit();
+	}
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+		
+		preferences = getSharedPreferences(ParallaxWallpaper.SHARED_PREFS_NAME, 0);
+		preferences.registerOnSharedPreferenceChangeListener(this);
+		
+		selectedImage = (TextView)findViewById(R.id.selectedImage);
+		updateSelectedImageUi();
+		
+		pickButton = (Button)findViewById(R.id.selectLayersButton);
+		pickButton.setOnClickListener(new View.OnClickListener() { public void onClick(View v) {
+			Intent anyFile = new Intent(Intent.ACTION_GET_CONTENT);
+            anyFile.setType("image/*");
+            anyFile.addCategory(Intent.CATEGORY_OPENABLE);
+            if (anyFile.resolveActivityInfo(getPackageManager(), 0) == null) {
+                new AlertDialog.Builder(context)
+                	.setTitle(R.string.no_image_picker_alert_title)
+                	.setMessage(R.string.no_image_picker_alert_message)
+                	.create().show();
+            } else {
+                Intent chooser = Intent.createChooser(anyFile, "Pick the first layer");
+                startActivityForResult(chooser, REQ_CODE_PICK_IMAGE);
             }
-        });
-        customPathPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                return processNewPath(newValue);
-            }
-        });
-        
-    }
-    
-    private boolean processNewPath(Object newValue) {
-        boolean alright = false;
-        String value = (String) newValue;
-        File file = new File(value);
-        if (!file.exists()) {
-            failedPathSet("File doesn't exist");
-        } else if (!file.isFile()) {
-            failedPathSet("Not a file");
-        } else if (!file.canRead()) {
-            failedPathSet("This file is unreadable");
-        } else {
-            Editor editor = preferences.edit();
-            editor.putString(CUSTOM_PATH_ACTUAL_KEY, value);
-            editor.commit();
-            alright = true;
-            customPathPreference.setSummary(value);
-        }
-        return alright;
-    }
+		}});
+	}
+	
+	private void updateSelectedImageUi() {
+		String value = getLayerPath();
+		if (value != null) {
+			selectedImage.setVisibility(View.VISIBLE);
+			// TODO i18n
+			selectedImage.setText(Html.fromHtml("Currently chosen: <br><b>" + value +"</b>"));
+		}
+	}
 
-    private void failedPathSet(String string) {
-        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
-        resetPathPreference();
-    }
+	private void processNewPath(String filePath) {
+		Log.d(TAG, "new file path " + filePath);
+		setLayerPath(filePath);
+	}
 
-    private void resetPathPreference() {
-        customPathPreference.setText(Environment.getExternalStorageDirectory().getAbsolutePath());
-    }
-
-
-
-    private boolean hasText(String text) {
-        return text != null && text.trim().length() > 0;
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(
-                this);
-        super.onDestroy();
-    }
-
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-            String key) {
-    }
-    
-    @Override
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		Log.d(TAG, "onSharedPreferenceChanged("+key+")");
+		if (key.equals(CUSTOM_PATH_ACTUAL_KEY)) {
+			updateSelectedImageUi();
+		}
+	}
+	
+	@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent returnedIntent) { 
         super.onActivityResult(requestCode, resultCode, returnedIntent); 
-
         switch(requestCode) { 
-        case REQ_CODE_PICK_IMAGE:
-            if(resultCode == RESULT_OK){  
-                Uri selectedImage = returnedIntent.getData();
-                Log.i(ParallaxWallpaper.TAG, "Picked image " + selectedImage);
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-                Cursor cursor = null;
-                try {
-                    cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    String filePath = cursor.getString(columnIndex);
-                    
-                    Log.i(ParallaxWallpaper.TAG, "Chose image " + filePath);
-                    customPathPreference.setText(filePath);
-                    customPathPreference.getEditText().setText(filePath);
-                    processNewPath(filePath);
-                } finally {
-                    if (cursor != null) cursor.close();
-                }
-
-            }
-            break;
+	        case REQ_CODE_PICK_IMAGE:
+	            if(resultCode == RESULT_OK){  
+	                Uri selectedImage = returnedIntent.getData();
+	                Log.i(ParallaxWallpaper.TAG, "Picked image " + selectedImage);
+	                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+	
+	                Cursor cursor = null;
+	                try {
+	                    cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+	                    cursor.moveToFirst();
+	                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	                    final String filePath = cursor.getString(columnIndex);
+	                    processNewPath(filePath);
+	                } finally {
+	                    if (cursor != null) cursor.close();
+	                }
+	
+	            }
+	            break;
         }
     }
+	
 }
--- src/uk/co/halfninja/wallpaper/parallax/ParallaxWallpaperSettingsHome.java
+++ src/uk/co/halfninja/wallpaper/parallax/ParallaxWallpaperSettingsHome.java
@@ -5,45 +5,112 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import static uk.co.halfninja.wallpaper.parallax.ParallaxWallpaper.TAG;
 
-public class ParallaxWallpaperSettingsHome extends Activity {
+public class ParallaxWallpaperSettingsHome extends Activity implements OnSharedPreferenceChangeListener {
 	private static final String CUSTOM_PATH_KEY = "custom_path";
     private static final String CUSTOM_PATH_ACTUAL_KEY = "custom_path_actual";
     private static final int REQ_CODE_PICK_IMAGE = 100001;
     
     private final Context context = this; // for convenience in inner classes
     
-    private EditTextPreference customPathPreference;
     private SharedPreferences preferences;
-
 	private Button pickButton;
+	private TextView selectedImage;
+	
+	public String getLayerPath() {
+		return preferences.getString(CUSTOM_PATH_ACTUAL_KEY, null);
+	}
+	
+	public void setLayerPath(String path) {
+		preferences.edit().putString(CUSTOM_PATH_ACTUAL_KEY, path).commit();
+	}
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
 		
-		preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		preferences = getSharedPreferences(ParallaxWallpaper.SHARED_PREFS_NAME, 0);
+		preferences.registerOnSharedPreferenceChangeListener(this);
+		
+		selectedImage = (TextView)findViewById(R.id.selectedImage);
+		updateSelectedImageUi();
 		
 		pickButton = (Button)findViewById(R.id.selectLayersButton);
 		pickButton.setOnClickListener(new View.OnClickListener() { public void onClick(View v) {
 			Intent anyFile = new Intent(Intent.ACTION_GET_CONTENT);
             anyFile.setType("image/*");
             anyFile.addCategory(Intent.CATEGORY_OPENABLE);
-            
             if (anyFile.resolveActivityInfo(getPackageManager(), 0) == null) {
-                AlertDialog alert = new AlertDialog.Builder(context)
+                new AlertDialog.Builder(context)
                 	.setTitle("No activity to pick an image")
-                	.setMessage(R.string.no_image_picker_alert_message);
-                	.create();
+                	.setMessage(R.string.no_image_picker_alert_message)
+                	.create().show();
             } else {
                 Intent chooser = Intent.createChooser(anyFile, "Pick the first layer");
                 startActivityForResult(chooser, REQ_CODE_PICK_IMAGE);
             }
 		}});
 	}
+	
+	private void updateSelectedImageUi() {
+		String value = getLayerPath();
+		if (value != null) {
+			selectedImage.setVisibility(View.VISIBLE);
+			// TODO i18n
+			selectedImage.setText(Html.fromHtml("Currently chosen: <br><b>" + value +"</b>"));
+		}
+	}
+
+	private void processNewPath(String filePath) {
+		Log.d(TAG, "new file path " + filePath);
+		setLayerPath(filePath);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		Log.d(TAG, "onSharedPreferenceChanged("+key+")");
+		if (key.equals(CUSTOM_PATH_ACTUAL_KEY)) {
+			updateSelectedImageUi();
+		}
+	}
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent returnedIntent) { 
+        super.onActivityResult(requestCode, resultCode, returnedIntent); 
+        switch(requestCode) { 
+	        case REQ_CODE_PICK_IMAGE:
+	            if(resultCode == RESULT_OK){  
+	                Uri selectedImage = returnedIntent.getData();
+	                Log.i(ParallaxWallpaper.TAG, "Picked image " + selectedImage);
+	                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+	
+	                Cursor cursor = null;
+	                try {
+	                    cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+	                    cursor.moveToFirst();
+	                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	                    final String filePath = cursor.getString(columnIndex);
+	                    processNewPath(filePath);
+	                } finally {
+	                    if (cursor != null) cursor.close();
+	                }
+	
+	            }
+	            break;
+        }
+    }
+	
 }
